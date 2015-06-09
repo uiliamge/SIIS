d42943466881b85b7126c79b75da02d93de92797
uiliamge <uiliamge@gmail.com>
2015-06-09 15:15:54 -0300

    Pagina inicial responsavel.

--- SIIS/Scripts/Home/Home.js
+++ SIIS/Scripts/Home/Home.js
@@ -6,8 +6,6 @@ function AbrirExtrato(idExtrato) {
 
     waitingDialog.show('Abrindo Extrato ' + idExtrato, { dialogSize: 'sm', progressType: 'success' });
 
-    var extrato = $("#formPreencherExtrato").serialize();
-
     $.ajax({
         type: "GET",
         url: $("#urlAbrirExtrato").val(),
@@ -15,6 +13,30 @@ function AbrirExtrato(idExtrato) {
         datatype: "html",
         success: function (data) {
             $("#bodyLayout").html(data);
+            waitingDialog.hide();
+        }
+    });
+};
+
+function FiltrarExtratosResponsavel(idResponsavel) {
+
+    waitingDialog.show('Consultando os seus extratos e filtrando', { dialogSize: 'sm', progressType: 'success' });
+
+    $.ajax({
+        type: "POST",
+        url: $("#urlFiltrarExtratosResponsavel").val(),
+        data: {
+            idResponsavel: idResponsavel,
+            codigo: $("#FiltroCodigo").val(),
+            cpf: $("#FiltroCpf").val(),
+            dataInicio: $("#FiltroDataInicio").val(),
+            dataFim: $("#FiltroDataFim").val(),
+            nome: $("#FiltroNome").val(),
+            cidade: $("#FiltroCidade").val()
+        },
+        datatype: "html",
+        success: function (data) {
+            $("#divExtratosResponsavel").html(data);
             waitingDialog.hide();
         }
     });
